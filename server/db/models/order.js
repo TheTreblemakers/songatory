1c44995a68839a20661ba8a5a21f8cbc0c6297b2
Jonathan Kammo <jonathankammo@gmail.com>
2017-09-12 17:04:33 -0500

    Define Order db model

---
+++ server/db/models/order.js
@@ -0,0 +1,35 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Order = db.define('order', {
+  date: {
+    type: Sequelize.DATE
+  },
+  session: {
+    type: Sequelize.STRING
+  },
+  fulfilled: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false
+  },
+  paymentMethod: {
+    type: Sequelize.STRING
+  }
+})
+
+module.exports = Order
+
+/**
+ * instanceMethods
+ */
+
+
+/**
+ * classMethods
+ */
+
+
+/**
+ * hooks
+ */
+
