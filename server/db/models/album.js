24a20210bc41af7a367885a9221f24bf6098600f
Jonathan Kammo <jonathankammo@gmail.com>
2017-09-13 10:05:23 -0500

    Add validation for Album db model regarding year set in the future

--- server/db/models/album.js
+++ server/db/models/album.js
@@ -12,7 +12,15 @@ const Album = db.define('album', {
     type: Sequelize.INTEGER // in cents! divide by 100 as appropriate
   },
   year: {
-    type: Sequelize.INTEGER
+    type: Sequelize.INTEGER,
+    validate: {
+      isValid(value) {
+        let currentYear = (new Date()).getFullYear();
+        if (value < 0 || value > currentYear) {
+          throw new Error('Please select a valid year! For instance, the album\'s year cannot be in the future');
+        }
+      }
+    }
   },
   image: {
     type: Sequelize.STRING,
