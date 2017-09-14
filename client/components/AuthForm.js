082300c715c552d472289514609d059776ed8ea0
Martin Gutfeldt <martingutfeldt@gmail.com>
2017-09-14 15:17:39 -0500

    Complete basic login/signup component

--- client/components/AuthForm.js
+++ client/components/AuthForm.js
@@ -2,38 +2,26 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { auth } from '../store';
+import { withRouter, Link } from 'react-router-dom';
+import { Container, Form, Message } from 'semantic-ui-react';
 
 const AuthForm = (props) => {
   const {name, displayName, handleSubmit, error} = props;
 
   return (
-    <div>
-      <form onSubmit={handleSubmit} name={name}>
-        <div>
-          <label htmlFor="email"><small>Email</small></label>
-          <input name="email" type="text" />
-        </div>
-        <div>
-          <label htmlFor="password"><small>Password</small></label>
-          <input name="password" type="password" />
-        </div>
-        <div>
-          <button type="submit">{displayName}</button>
-        </div>
-        {error && error.response && <div> {error.response.data} </div>}
-      </form>
+    <Container>
+      <h2>{displayName}</h2>
+      <Form onSubmit={handleSubmit} name={name}>
+        <Form.Input label="Email" name="email" type="email" />
+        <Form.Input label="Password" name="password" type="password" />
+        <Form.Button>Submit</Form.Button>
+        {error && error.response && <Message negative> {error.response.data} </Message>}
+      </Form>
       <a href="/auth/google">{displayName} with Google</a>
-    </div>
+    </Container>
   );
 };
 
-/**
- * CONTAINER
- *   Note that we have two different sets of 'mapStateToProps' functions -
- *   one for Login, and one for Signup. However, they share the same 'mapDispatchToProps'
- *   function, and share the same Component. This is a good example of how we
- *   can stay DRY with interfaces that are very similar to each other!
- */
 const mapLogin = (state) => {
   return {
     name: 'login',
@@ -53,6 +41,7 @@ const mapSignup = (state) => {
 const mapDispatch = (dispatch) => {
   return {
     handleSubmit (evt) {
+      // console.log(evt.target);
       evt.preventDefault();
       const formName = evt.target.name;
       const email = evt.target.email.value;
@@ -62,8 +51,8 @@ const mapDispatch = (dispatch) => {
   };
 };
 
-export const Login = connect(mapLogin, mapDispatch)(AuthForm);
-export const Signup = connect(mapSignup, mapDispatch)(AuthForm);
+export const Login = withRouter(connect(mapLogin, mapDispatch)(AuthForm));
+export const Signup = withRouter(connect(mapSignup, mapDispatch)(AuthForm));
 
 /**
  * PROP TYPES
