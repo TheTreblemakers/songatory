f065c84c9279ce1712a526e3bdadbe5199d2b2a1
Ashish Malav <aashmalav@gmail.com>
2017-09-14 10:38:51 -0500

    created branch for issue 34 - Albums Component

---
+++ client/store/album.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+import history from '../history';
+
+/**
+ * ACTION TYPES
+ */
+const GET_ALBUMS = 'GET_ALBUMS';
+
+/**
+ * INITIAL STATE
+ */
+const allAlbums = {};
+
+/**
+ * ACTION CREATORS
+ */
+const getAlbums = albums => ({type: GET_ALBUMS, albums});
+
+
+/**
+ * THUNK CREATORS
+ */
+export const fetchAlbums = () =>
+  dispatch =>
+    axios.get('/api/albums')
+      .then(res =>
+        dispatch(getAlbums(res.data || allAlbums)))
+      .catch(err => console.log(err));
+
+/**
+ * REDUCER
+ */
+export default function (state = allAlbums, action) {
+  switch (action.type) {
+    case GET_USER:
+      return action.user;
+    case REMOVE_USER:
+      return defaultUser;
+    default:
+      return state;
+  }
+}
