6e2736247c2cb54f612958d2702e5b039810b234
Ashish Malav <aashmalav@gmail.com>
2017-09-16 17:05:39 -0500

    merged master

--- client/store/cart.js
+++ client/store/cart.js
@@ -6,8 +6,7 @@ import history from '../history';
  */
 const GET_CART_ALBUMS = 'GET_CART_ALBUMS';
 const GET_CART_SONGS = 'GET_CART_SONGS';
-const ADD_CART_ALBUMS_TO_STATE = 'ADD_CART_ALBUMS_TO_STATE';
-const ADD_CART_SONGS_TO_STATE = 'ADD_CART_SONGS_TO_STATE';
+
 
 /**
  * INITIAL STATE
@@ -20,8 +19,6 @@ const cart = { albums: [], songs: [] };
 
 export const getCartAlbums = albums => ({type: GET_CART_ALBUMS, albums});
 export const getCartSongs = songs => ({type: GET_CART_SONGS, songs});
-export const addCartAlbumsToState = albums => ({type: ADD_CART_ALBUMS_TO_STATE, albums});
-export const addCartSongsToState = songs => ({type: ADD_CART_SONGS_TO_STATE, songs});
 
 /**
  * THUNK CREATORS
@@ -38,13 +35,13 @@ export const fetchCart = () =>
 export const addAlbumToCart = (album) =>
   dispatch =>
     axios.post(`/api/orders/cart/albums`, album)
-      .then(res => dispatch(addCartAlbumsToState(res.data ||      cart.albums)))
+      .then(res => dispatch(getCartAlbums(res.data ||      cart.albums)))
       .catch(err => console.log(err));
 
 export const addSongToCart = (song) =>
   dispatch =>
     axios.post(`/api/orders/cart/songs`, song)
-      .then(res => dispatch(addCartSongsToState(res.data || cart.songs)))
+      .then(res => dispatch(getCartSongs(res.data || cart.songs)))
       .catch(err => console.log(err));
 
 export const removeAlbumFromCart = (albumId) =>
@@ -70,12 +67,6 @@ export default function (state = cart, action) {
       newState.albums = action.albums;
       return newState;
     case GET_CART_SONGS:
-      newState.songs = action.songs;
-      return newState;
-    case ADD_CART_ALBUMS_TO_STATE:
-      newState.albums = action.albums;
-      return newState;
-    case ADD_CART_SONGS_TO_STATE:
       newState.songs = action.songs;
       return newState;
     default:
