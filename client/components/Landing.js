83503e66a50fcf2df53f8b54cb4ca232576b798e
Tim <theshuo@gmail.com>
2017-09-15 19:19:34 -0500

    Adds transition effect for landing title

--- client/components/Landing.js
+++ client/components/Landing.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Splash } from '../components';
-import { Header } from 'semantic-ui-react';
+import { Header, Transition } from 'semantic-ui-react';
 
 const Landing = (props) => {
   const styles = {
@@ -14,9 +14,11 @@ const Landing = (props) => {
 
   return (
     <Splash>
-      <Header inverted style={styles.title}>
-        songatory
-      </Header>
+      <Transition transitionOnMount={true} animation="fade up" duration={2000}>
+        <Header inverted style={styles.title}>
+          songatory
+        </Header>
+      </Transition>
     </Splash>
   );
 };
