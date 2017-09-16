e6b54501f9d31ecc34e80c09c1481e4c1e7e0d2b
Jonathan Kammo <jonathankammo@gmail.com>
2017-09-16 17:36:50 -0500

    Hard code a version of recent orders which includes nested product details

--- client/components/UserHome.js
+++ client/components/UserHome.js
@@ -27,21 +27,19 @@ class UserHome extends Component {
 
   render() {
     const { email, orders } = this.props;
-    console.log(this.props);
+    console.log('HEYYYYYY orders:  ', orders);
 
     return (
       <div>
         <h3>Welcome, {email}</h3>
         <Container >
-          <h2>Your Orders History</h2>
+          <h2>Your Recent Order History</h2>
           <Table striped>
             <Table.Header>
               <Table.Row>
-                <Table.HeaderCell>Product Name</Table.HeaderCell>
-                <Table.HeaderCell>Type</Table.HeaderCell>
+                <Table.HeaderCell>Order Id</Table.HeaderCell>
                 <Table.HeaderCell>Date</Table.HeaderCell>
-                <Table.HeaderCell>Status</Table.HeaderCell>
-                <Table.HeaderCell>Price</Table.HeaderCell>
+                <Table.HeaderCell>Product details</Table.HeaderCell>
               </Table.Row>
             </Table.Header>
 
@@ -49,10 +47,45 @@ class UserHome extends Component {
               {
                 orders.map(order => (
                   <Table.Row key={order.id}>
-                     <Table.Cell> order name</Table.Cell> 
+                    {/* <Table.Cell> order name</Table.Cell> */}
+                    <Table.Cell> {order.id}</Table.Cell>
                     <Table.Cell> {order.date}</Table.Cell>
-                     <Table.Cell>order.status</Table.Cell> 
-                     <Table.Cell>order.price</Table.Cell> 
+                    <Table.Cell>
+                      <Table >
+                        <Table.Header>
+                          <Table.Row>
+                            <Table.HeaderCell>Product Name</Table.HeaderCell>
+                            <Table.HeaderCell>Type</Table.HeaderCell>                               <Table.HeaderCell>Status</Table.HeaderCell>
+                            <Table.HeaderCell>Price</Table.HeaderCell>
+                          </Table.Row>
+                        </Table.Header>
+                        {/* <Table.Body>
+                          {orders.products.map(product => (
+                            <Table.Row key={product.id}>
+                              <Table.Cell> {product.name}</Table.Cell>
+                              <Table.Cell> {product.type}</Table.Cell>
+                              <Table.Cell> {product.status}</Table.Cell>
+                              <Table.Cell> {product.price}</Table.Cell>
+                            </Table.Row>
+                          ))} */}
+                        <Table.Body>
+                            <Table.Row key="test">
+                              <Table.Cell>test</Table.Cell>
+                              <Table.Cell>test</Table.Cell>
+                              <Table.Cell>test</Table.Cell>
+                              <Table.Cell>test</Table.Cell>
+                            </Table.Row>
+                            <Table.Row key="test2">
+                              <Table.Cell>test2</Table.Cell>
+                              <Table.Cell>test2</Table.Cell>
+                              <Table.Cell>test2</Table.Cell>
+                              <Table.Cell>test2</Table.Cell>
+                            </Table.Row>
+                        </Table.Body>
+                      </Table>
+                    </Table.Cell>
+                    {/* <Table.Cell>order.status</Table.Cell> */}
+                    {/* <Table.Cell>order.price</Table.Cell> */}
                   </Table.Row>
                 ))
               }
@@ -80,7 +113,7 @@ const mapState = (state) => {
   return {
     email: state.user.email,
     userId: state.user.id,
-    orders: []
+    orders: [{ id: 1234321, date: '12/21/2014', products: [{ productName: 'Bye Bye Bye', type: 'Song', status: 'Available', price: '$1.21' }, { productName: 'Bye Bye Bye', type: 'Album', status: 'Available', price: '$31.21' }, { productName: 'Genie in a bottle', type: 'Song', status: 'Available', price: '$1.24' }] }, { id: 1234322, date: '12/24/2014', products: [{ productName: 'Bye Bye Bye', type: 'Song', status: 'Available', price: '$1.21' }, { productName: 'Livin la vida loca', type: 'Song', status: 'Available', price: '$1.28' }] }]
   };
 };
 
@@ -98,5 +131,6 @@ export default connect(mapState, mapDispatch)(UserHome);
  * PROP TYPES
  */
 UserHome.propTypes = {
-  email: PropTypes.string
+  email: PropTypes.string,
+  orders: PropTypes.array,
 };
