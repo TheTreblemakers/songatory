e852a0cd18d259ee4cd05485f21427bb87f351ca
Martin Gutfeldt <martingutfeldt@gmail.com>
2017-09-16 11:25:01 -0500

    Improve loading and sending of cart

--- server/api/orders.js
+++ server/api/orders.js
@@ -2,9 +2,22 @@ const router = require('express').Router();
 const { Order } = require('../db/models');
 module.exports = router;
 
+// Load cart
+router.use('/cart/', (req, res, next) => {
+  if (!req.user) {
+    res.sendStatus(404);
+  } else {
+    Order.findOne({ where: { userId: req.user.id, fulfilled: false } })
+      .then(order => order ? order : Order.create({ userId: req.user.id }))
+      .then(order => {
+        req.order = order;
+        next();
+      })
+      .catch(next);
+  }
+});
+
 // GET /api/orders/cart/
 router.get('/cart/', (req, res, next) => {
-  Order.findOne({ where: { userId: req.user.id, fulfilled: false } })
-    .then(order => res.json(order || {}))
-    .catch(next);
+  res.json({ albums: req.order.albums, songs: req.order.songs });
 });
