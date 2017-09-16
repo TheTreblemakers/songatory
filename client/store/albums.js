4e0e9bbf233f77e4e41542ab3cd7e21f0e4ecf0a
Tim <theshuo@gmail.com>
2017-09-16 11:48:51 -0500

    Refactors Albums and adds single Album view

--- client/store/albums.js
+++ client/store/albums.js
@@ -5,35 +5,58 @@ import history from '../history';
  * ACTION TYPES
  */
 const GET_ALBUMS = 'GET_ALBUMS';
+const GET_ALBUM = 'GET_ALBUM';
 
 /**
  * INITIAL STATE
  */
-const allAlbums = [];
+const initialState = {
+  allAlbums: [],
+  currentAlbum: {},
+};
 
 /**
  * ACTION CREATORS
  */
-export const getAlbums = albums => ({type: GET_ALBUMS, albums});
+export const getAlbums = (albums) => {
+  return { type: GET_ALBUMS, albums };
+};
 
+export const getAlbum = (album) => {
+  return { type: GET_ALBUM, album };
+};
 
 /**
  * THUNK CREATORS
  */
-export const fetchAlbums = () =>
-  dispatch =>
-    axios.get('/api/albums')
-      .then(res =>
-        dispatch(getAlbums(res.data || allAlbums)))
-      .catch(err => console.log(err));
+export const fetchAlbums = () => (dispatch) =>
+  axios
+    .get('/api/albums')
+    .then((res) => {
+      dispatch(getAlbums(res.data));
+    })
+    .catch((err) => console.log(err));
+
+export const fetchAlbum = (id) => (dispatch) =>
+  axios
+    .get(`/api/albums/${id}`)
+    .then((res) => {
+      dispatch(getAlbum(res.data));
+    })
+    .catch((err) => console.log(err));
 
 /**
  * REDUCER
  */
-export default function (state = allAlbums, action) {
+export default function(state = initialState, action) {
+  const newState = Object.assign({}, state);
   switch (action.type) {
     case GET_ALBUMS:
-      return action.albums;
+      newState.allAlbums = action.albums;
+      return newState;
+    case GET_ALBUM:
+      newState.currentAlbum = action.album;
+      return newState;
     default:
       return state;
   }
